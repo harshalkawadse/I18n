01d21210059f2a708385eab2c6dbbf904a15ca81
harshalkawadse <harshalkawadse2003@gmail.com>
2024-03-14 16:07:29 +0530

    changes has done

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import "./Home.css"
-
+import I18n from "./../../utils/I18n";
 function Home() {
-    return (
+const usersCount = 2500;
+
+ return (
         <div>
-            Home
-        </div>
+            <h1>  {I18n("welcomeMessage")}  </h1>
+
+            <h2>  {I18n("normalMessage")} </h2>
+
+            <h3>  {I18n("greetingMessage")}  </h3>
+
+              <h2>
+               {I18n("usersStatmessage","<studentcount>",usersCount)}
+
+             </h2>
+
+            <h5> {I18n("endMessage")}</h5>
+
+ <select 
+            defaultValue={localStorage.getItem("lang")}
+            onChange={(e)=>{
+                localStorage.setItem("lang",e.target.value);
+                window.location.reload();
+
+            }}>
+                <option value="mr">Marathi</option>
+                <option value="hi">Hindi</option>
+                <option value="en">English</option>
+ </select>
+</div>
     )
 }
-
 export default Home
